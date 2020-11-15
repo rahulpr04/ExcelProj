5f4ffb8bd35b826e216d3dc694e2026945dfbcc4
Rahul Prasad <rahulprasad04@gmail.com>
2020-11-15 20:49:43 +0000

    Updated program with panda/excel pokemon tutorial

--- prog.py
+++ prog.py
@@ -39,3 +39,38 @@ print(df.head(5))
 df.to_csv('./Data/Modified.csv',index=False)
 df.to_excel('./Data/modifiedxls.xlsx',index=False)
 
+print ("Above 300 Total, and Grass and Poison:")
+above300=df.loc[(df['Type 1'] == 'Grass') & (df['Type 2'] == 'Poison') & (df['Total2']>300)]
+print(above300)
+
+print ("Below 300 Total:")
+below300=df.loc[df['Total2']<300]
+print(below300)
+
+#To reset index:
+above300 = above300.reset_index(drop=True)
+#print(above300)
+
+#Subjective changes (Multiple)
+df.loc[df['Type 1'] == 'Fire', ['Legendary', 'Generation']] = [True, '0']
+#print(df)
+
+#Groupby (aggregate statistics)
+group=df.groupby(['Type 1']).mean().sort_values('Defense',ascending=False)
+print(group)
+group=df.groupby(['Type 1']).sum()
+print(group)
+group=df.groupby('Type 1').count()
+print(group)
+
+#Create new empty dataframe with same columns
+new_df = pd.DataFrame(columns=df.columns)
+
+#Working with large amounts of data
+new_df=new_df
+for df in pd.read_csv('./Data/pokemon_data.csv', chunksize=5):
+   print("Chunk dataframe:")
+   print(df)
+   new_df = pd.concat([new_df,df])
+print(df)
+print(new_df.head(50))
